8879e5d4465eb4ab19737c1ce1cd7e5e9e418159
Thiago Soares <tcsoares84@gmail.com>
2023-06-19 05:08:38 -0300

    feat: make configs and set swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from '@src/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Parking API')
+    .setDescription('Manage vehicles into parking.')
+    .setVersion(process.env.npm_package_version)
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const port = process.env.APPLICATION_PORT;
+  await app.listen(port);
+  Logger.log(`🔥 Server running on http://localhost:${port}/`, 'NestBootstrap');
 }
 bootstrap();
